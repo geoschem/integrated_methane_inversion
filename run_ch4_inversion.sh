1c041ce012d28ad7b47786f4500f863cc7a96514
laestrada <63303345+laestrada@users.noreply.github.com>
2022-02-02 13:16:30 -0500

    Sbatch bugfix: enable running end to end script with sbatch (#28)
    * handle using sbatch to run end to end script
    
    * added description to parse_yaml file

--- run_ch4_inversion.sh
+++ run_ch4_inversion.sh
@@ -12,10 +12,14 @@ start_time=$(date)
 
 printf "\n=== PARSING CONFIG FILE ===\n"
 
-# Function to parse yaml files from shell script
-# By Stefan Farestam via stackoverflow:
-# https://stackoverflow.com/questions/5014632/how-can-i-parse-a-yaml-file-from-a-linux-shell-script
-function parse_yaml {
+# Description: Function to parse yaml files from shell script
+#   By Stefan Farestam via stackoverflow:
+#   https://stackoverflow.com/questions/5014632/how-can-i-parse-a-yaml-file-from-a-linux-shell-script
+# Usage:
+#   parse_yaml <file-path> <local-prefix>
+#      <file-path>: path to yaml file you would like to parse
+#      <local-prefix>: [Optional] prefix to append onto yaml vars
+parse_yaml() {
    local prefix=$2
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
    sed -ne "s|^\($s\):|\1|" \
@@ -26,10 +30,29 @@ function parse_yaml {
       vname[indent] = $2;
       for (i in vname) {if (i > indent) {delete vname[i]}}
       if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+        vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+        printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
       }
    }'
+}
+
+# Description: running `sbatch <file>; wait;` within a preexisting sbatch 
+#   process on aws results in the child sbatch job getting stuck in pending 
+#   because all available resources on the instance are utilized by the 
+#   driving sbatch command. In this case the child sbatch process 
+#   should be run directly. This function schedules a job either by using 
+#   sbatch or direct call depending on whether the parent process is using slurm
+# Usage:
+#   scheduleJob <runscript-name>
+#      runscript-name: script to schedule or run job for
+scheduleJob() {
+
+    if "$UseSlurm" && "$isAWS"; then
+        ./$1
+    else
+        # Submit job to job scheduler
+        sbatch -W $1; wait;
+    fi
 }
 
 # Get configuration
@@ -106,7 +129,7 @@ if  "$DoSpinup"; then
     fi
 
     # Submit job to job scheduler
-    sbatch -W ${RunName}_Spinup.run; wait;
+    scheduleJob ${RunName}_Spinup.run
 
     printf "=== DONE SPINUP SIMULATION ===\n"
     
@@ -153,7 +176,7 @@ if "$DoInversion"; then
     fi
 
     # Execute inversion driver script
-    sbatch -W run_inversion.sh; wait;
+    scheduleJob run_inversion.sh
         
     printf "=== DONE RUNNING INVERSION ===\n"
 
@@ -175,7 +198,7 @@ if "$DoPosterior"; then
 
     # Submit job to job scheduler
     printf "\n=== SUBMITTING POSTERIOR SIMULATION ===\n"
-    sbatch -W ${RunName}_Posterior.run; wait;
+    scheduleJob ${RunName}_Posterior.run
     printf "=== DONE POSTERIOR SIMULATION ===\n"
 
     cd ${MyPath}/${RunName}/inversion
