93a2b328272fe75bd4e6e6fcf405d7d988732587
laestrada <laestrada00@gmail.com>
2022-02-17 10:19:03 -0500

    added comments to the scripts

--- src/utilities/find_corrupt_files.sh
+++ src/utilities/find_corrupt_files.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
-# Arguments:
-#   s3_path: path of s3 files to recursively download
-#   dest: path files will be downloaded to
-download_aws_files() {
-    aws s3 cp $1 $2 --request-payer --recursive --dryrun
-}
+# This shell script is used to verify the integrity of 
+# netcdf s3 datasets. It is meant to download the 
+# specified data from s3 and run an nccopy command to
+# verify the file is in the correct format. All corrupted 
+# files are written out to a txt file
 
-report() {
-  corrupt=1
-} >&2
-
-# Configuration
+# ************** Configuration **************************
 S3_BUCKET=gcgrid
 RESOLUTION=GEOS_0.25x0.3125_AS
 MET=GEOS_FP
 YEAR=2019
 DEST="/home/ubuntu/ExtData/${RESOLUTION}/${MET}/${YEAR}/"
 S3_PATH="s3://${S3_BUCKET}/${RESOLUTION}/${MET}/${YEAR}/"
+# ************** Configuration End **********************
+
+# Usage: download specified s3 data recursively
+# Arguments:
+#   s3_path: path of s3 files to recursively download
+#   dest: path files will be downloaded to
+download_aws_files() {
+    echo "starting aws download"
+    aws s3 cp $1 $2 --request-payer --recursive --dryrun
+    echo "finished aws download"
+}
+
+# Usage: report errors from the corruption test
+report() {
+  corrupt=1
+} >&2
 
 mkdir -p $DEST
 download_aws_files $S3_PATH $DEST
 
+# trap will automatically send any error exit codes to the 
+# report function which sets the corruption boolean to 1 (True)
 corrupt=0
 trap report ERR
 
-FILE_LIST=$(find "${DEST}" -type f)
-echo $DEST
+# Get a sorted list of files from the destination directory
+FILE_LIST=$(find "${DEST}" -type f | sort)
+echo "starting file corruption testing"
+
+# Iterate through the list and test whether each file successfully 
+# performs the nccopy command
 for file in $FILE_LIST; do
     echo "testing file: ${file}"
     nccopy $file testfile.nc
@@ -37,6 +54,7 @@ for file in $FILE_LIST; do
     fi
     rm -f testfile.nc
 done
+echo "Finished file corruption testing for specified data"
 
 
 
