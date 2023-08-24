bug fixes for 2x2.5, parallelize jacobian and faster calc_sensi

src/components/statevector_component/aggregation.py
@@ -225,13 +225,13 @@ def get_max_aggregation_level(config, sensitivities, desired_element_num):
     Returns:                  int : max gridcells per cluster
     """
     if config["Res"] == "2x2.5":
-        max_aggregation_level = 4 # confirm
+        max_aggregation_level = 4
     elif config["Res"] == "0.25x0.3125":
         max_aggregation_level = 256
     elif config["Res"] == "0.5x0.625":
         max_aggregation_level = 64
 
-    background_elements_needed = np.ceil(len(sensitivities) / max_aggregation_level)
+    background_elements_needed = np.ceil(len(sensitivities) / max_aggregation_level) # 1-month: 10800/4 = 2700 > 600
     if background_elements_needed > desired_element_num:
         print(
             "Warning: too few clusters to create a background of 4x5 degree state vector elements."

src/inversion_scripts/calc_sensi.py
@@ -87,24 +87,34 @@ def calc_sensi(
         nlon = len(base_data["lon"])  # 52
         nlat = len(base_data["lat"])  # 61
         nlev = len(base_data["lev"])  # 47
+        base_var = base_data["SpeciesConcVV_CH4"] # Read base data before loop
+
+        # Save this data into numpy array so we don't need to read files in loop
+        pert_datas = []
+         # For each state vector element
+        for e in elements:
+            # State vector elements are numbered 1..nelements
+            elem = zero_pad_num(e + 1)
+            # Load the SpeciesConc file for the current element and day
+            pert_data = xr.open_dataset(
+                f"{run_dirs_pth}/{run_name}_{elem}/OutputDir/GEOSChem.SpeciesConc.{d}_0000z.nc4",
+                chunks='auto'
+            )
+            pert_datas.append(pert_data)
+            pert_data.close()
 
         # For each hour
         def process(h):
             # Get the base run data for the hour
-            base = base_data["SpeciesConcVV_CH4"][h, :, :, :]
+            # base = base_data["SpeciesConcVV_CH4"][h, :, :, :]
+            base = base_var[h, :, :, :]
             # Initialize sensitivities array
             sensi = np.empty((nelements, nlev, nlat, nlon))
             sensi.fill(np.nan)
             # For each state vector element
             for e in elements:
-                # State vector elements are numbered 1..nelements
-                elem = zero_pad_num(e + 1)
-                # Load the SpeciesConc file for the current element and day
-                pert_data = xr.load_dataset(
-                    f"{run_dirs_pth}/{run_name}_{elem}/OutputDir/GEOSChem.SpeciesConc.{d}_0000z.nc4"
-                )
                 # Get the data for the current hour
-                pert = pert_data["SpeciesConcVV_CH4"][h, :, :, :]
+                pert = pert_datas[e]["SpeciesConcVV_CH4"][h, :, :, :]
                 # Compute and store the sensitivities
                 sensitivities = (pert.values - base.values) / perturbation
                 sensi[e, :, :, :] = sensitivities