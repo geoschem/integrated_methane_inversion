bug fixes to work with 2x2.5, parallelization of jacobian and faster calc_sensi

src/components/statevector_component/aggregation.py
@@ -224,7 +224,9 @@ def get_max_aggregation_level(config, sensitivities, desired_element_num):
         desired_element_num   int : desired number of state vector elements
     Returns:                  int : max gridcells per cluster
     """
-    if config["Res"] == "0.25x0.3125":
+    if config["Res"] == "2x2.5":
+        max_aggregation_level = 4 # confirm
+    elif config["Res"] == "0.25x0.3125":
         max_aggregation_level = 256
     elif config["Res"] == "0.5x0.625":
         max_aggregation_level = 64
@@ -238,7 +240,7 @@ def get_max_aggregation_level(config, sensitivities, desired_element_num):
         # if there are too few clusters then we set the max aggregation level
         # to either total_native_elements/8 or total_native_elements
         denominator = 8 if desired_element_num > 8 else 1
-        max_aggregation_level = np.ceil(len(sensitivities) / denominator)
+        max_aggregation_level = np.ceil(len(sensitivities) / denominator).astype(int)
         print(
             f"Max aggregation level set to: {max_aggregation_level} elements in a cluster"
         )
@@ -316,7 +318,10 @@ def force_native_res_pixels(config, clusters, sensitivities):
         )
         return sensitivities
 
-    if config["Res"] == "0.25x0.3125":
+    if config["Res"] == "2x2.5":
+        lat_step = 2
+        lon_step = 2.5
+    elif config["Res"] == "0.25x0.3125":
         lat_step = 0.25
         lon_step = 0.3125
     elif config["Res"] == "0.5x0.625":