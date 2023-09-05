b94591e791ac8ff75c74add7c29e393dde373df4
nicholasbalasus <nicholasbalasus@g.harvard.edu>
2023-09-05 19:45:57 -0400

    start to rewrite run_BCs.sh, add restart file

--- src/write_BCs/run_BCs.sh
+++ src/write_BCs/run_BCs.sh
@@ -1,81 +1,99 @@
 #!/bin/bash
+#SBATCH --job-name=boundaryConditions
+#SBATCH --mem=4000
+#SBATCH --time=07-00:00
+#SBATCH --output=debug.log
+#SBATCH --partition=huce_cascade
 
-# Read in the config file
+cwd="$(pwd)"
+
+# Read in the config file and source the environment file
 source ../utilities/parse_yaml.sh
 eval $(parse_yaml config_write_BCs.yml)
+source ${GEOSChemEnv}
+echo "Environment file  --> ${GEOSChemEnv}" >> "${cwd}/boundaryConditions.log"
 
-# Make directories if they don't exists (and throw an error if they are not empty so we don't accidentally overwrite anything)
-function create_and_check_dirs() {
-    local dirs=("$@")
-    for dir in "${dirs[@]}"; do
-        mkdir -p "$dir"
-        if [[ $(find "$dir" -mindepth 1 -print -quit) ]]; then
-            echo "Error: Directory $dir contains files."
-            exit 1
-        fi
-    done
-}
+# As long as it doesn't exist, make the working directory and go to it
+if [[ -d "${workDir}" ]]; then
+    echo "ERROR             --> Directory ${workDir} exists." >> "${cwd}/boundaryConditions.log"
+    exit 1
+fi
+mkdir -p "${workDir}"
+echo "Working directory --> ${workDir}" >> "${cwd}/boundaryConditions.log"
+cd "${workDir}"
 
-if "$RunGEOSChem"; then
-    # Load modules
-    source ${GEOSChemEnv}
+# Get GCClassic v14.2.1 and create the run directory
+git clone https://github.com/geoschem/GCClassic.git
+cd GCClassic
+git checkout dac5a54 # most recent dev/14.2.1 @ 1 Sep 2023 12:44 PM (update this once 14.2.1 officially released)
+git submodule update --init --recursive
+cd run
+runDir="gc_run"
+c="3\n2\n2\n2\n${workDir}\n${runDir}\nn\n" # CH4, GEOS-FP, 2.0 x 2.5, 47L
+printf ${c} | ./createRunDir.sh
+cd "${workDir}/${runDir}/build"
+cmake ../CodeDir -DRUNDIR=..
+make -j
+make install
+cd "${workDir}/${runDir}"
 
-    # Make sure runGCC1402 is empty so we don't accidentally overwrite it
-    dirs=("${workdir}/runGCC1402")
-    create_and_check_dirs "${dirs[@]}"
+# Modify HISTORY.rc (hourly instantaneous CH4/pressure and 3-hourly BCs)
+sed -i -e "s|'CH4',|#'CH4',|g" \
+    -e "s|'Metrics',|#'Metrics',|g" \
+    -e "s|'StateMet',|#'StateMet',|g" \
+    -e "s|#'LevelEdgeDiags',|'LevelEdgeDiags',|g" \
+    -e "s|Restart.frequency:          'End',|Restart.frequency:          '00000100 000000',|g" \
+    -e "s|Restart.duration:           'End',|Restart.duration:           '00000100 000000',|g" \
+    -e "s|SpeciesConc.frequency:      00000100 000000|SpeciesConc.frequency:      00000000 010000|g" \
+    -e "s|SpeciesConc.duration:       00000100 000000|SpeciesConc.duration:       00000001 000000|g" \
+    -e "s|SpeciesConc.mode:           'time-averaged'|SpeciesConc.mode:           'instantaneous'|g" \
+    -e "s|'SpeciesConcMND_?ALL?          ',|#'SpeciesConcMND_?ALL?          ',|g" \
+    -e "s|LevelEdgeDiags.frequency:   00000100 000000|LevelEdgeDiags.frequency:   00000000 010000|g" \
+    -e "s|LevelEdgeDiags.duration:    00000100 000000|LevelEdgeDiags.duration:    00000000 010000|g" \
+    -e "s|LevelEdgeDiags.mode:        'time-averaged'|LevelEdgeDiags.mode:        'instantaneous'|g" \
+    -e "s|#'BoundaryConditions',|'BoundaryConditions',|g" HISTORY.rc
 
-    # Remove GCClassic if it exists
-    rm -rf ${workdir}/runGCC1402
-    rm -rf ${imidir}/src/write_BCs/GCClassic
+# Modify geoschem_config.yml
+# - run GC earlier than you want BCs to accomodate a 15 day average going back in time
+# - e.g., the BCs for 15 May 2023 require 1 May 2023-15 May 2023 data
+if [[ ${startDate} -ge "20180415" ]]; then
+    gcStartDate=$(date -d "$startDate -14 days" +%Y%m%d)
+else
+    gcStartDate="20180401"
+fi
+# - run GC to 00:00:00 the day after you want BCs
+# - e.g., you want BCs for 31 Aug 2023 -> run GC to 1 Sep 2023 00:00:00
+gcEndDate=$(date -d "$endDate +1 days" +%Y%m%d)
+sed -i -e "s|start_date: \[20190101, 000000\]|start_date: [${gcStartDate}, 000000]|g" \
+    -e "s|end_date: \[20190201, 000000\]|end_date: [${gcEndDate}, 000000]|g" geoschem_config.yml
+echo "GC run times        --> ${gcStartDate} 00:00:00 until ${gcEndDate} 00:00:00" >> "${cwd}/boundaryConditions.log"
 
-    # Download GEOS-Chem 14.0.2
-    git clone https://github.com/geoschem/GCClassic.git
-    cd GCClassic
-    git checkout 14.0.2
-    git submodule update --init --recursive
-    cd run
-    cmd="3\n2\n1\n2\n${workdir}\nrunGCC1402\nn\n" # these defaults will be replaced by the files in GC_config_files anyway
-    printf ${cmd} | ./createRunDir.sh >> createRunDir.log 2>&1
-    rm -f createRunDir.log
-
-    # Overwrite the default config files
-    cd ${imidir}/src/write_BCs/GC_config_files
-    cp geoschem_config.yml ${workdir}/runGCC1402/
-    cp HEMCO_Config.rc ${workdir}/runGCC1402/
-    cp HISTORY.rc ${workdir}/runGCC1402/
-
-    # Modify geoschem_config.yml to match our start and end date specified in the config file
-    # e.g., if you want BCs for 20180401-20230331, run GC for 20180401T000000-20230420T000000 (accounts for +/- 15 day temporal averaging in calculate_bias.py)
-    cd ${workdir}/runGCC1402
-    gc_enddate=$(date -d "$enddate +20 days" +%Y%m%d)
-    sed -i -e "s|start_date: \[[ ]*.*[ ]*\]|start_date: \[${startdate}, 000000\]|g"\
-           -e "s|end_date: \[[ ]*.*[ ]*\]|end_date: \[${gc_enddate}, 000000\]|g" geoschem_config.yml
-
-    # Compile GEOS-Chem
-    cd ${workdir}/runGCC1402/build
-    cmake ${imidir}/src/write_BCs/GCClassic >> build_geoschem.log 2>&1
-    cmake . -DRUNDIR=..  >> build_geoschem.log 2>&1
-    make -j install >> build_geoschem.log 2>&1
-
-    # Run GEOS-Chem
-    cd ${workdir}/runGCC1402/
-    cp ${RestartDir}/GEOSChem.Restart.${startdate:0:8}_0000z.nc4 ./Restarts/
-    sbatch -W ${imidir}/src/write_BCs/GC_config_files/geoschem.run; wait;
+# Prepare the restart file
+rm Restarts/GEOSChem.Restart.20190101_0000z.nc4
+if [[ ${startDate} -eq "20180401" ]]; then #  use the restart file provided with the IMI
+    cp "${cwd}/GEOSChem.Restart.20180401_0000z.nc4" Restarts/
+elif [[ -e ${restartFilePath} ]]; then # use your own restart file
+    restartFileDate=$(echo "${restartFilePath}" | grep -oP '\d{8}')
+    if [[ ${restartFileDate} -ne ${gcStartDate} ]]; then
+        echo "ERROR             --> your restart file date (${restartFileDate}) \
+              doesn't match the simulation start date (${gcStartDate})" >> "${cwd}/boundaryConditions.log"
+        exit 1
+    fi
+    cp "${restartFilePath}" Restarts/
+else
+    echo "ERROR             --> the restart file does not exist!" >> "${cwd}/boundaryConditions.log"
+    exit 1
 fi
 
-if "$WriteBCs"; then
-
-    # Make sure dirs are empty so we don't accidentally overwrite them
-    dirs=("${workdir}/step1" "${workdir}/step2" "${workdir}/step3" "${workdir}/smoothed-boundary-conditions")
-    create_and_check_dirs "${dirs[@]}"
-    
-    # Run python scripts
-    cd ${imidir}/src/write_BCs
-    sbatch -W -p ${Partition} -t 2-00:00 --mem 184000 -c 48 --wrap "source ~/.bashrc; conda activate $CondaEnv; python write_tropomi_GC_daily_avgs.py"; wait;
-    sbatch -W -p ${Partition} -t 2-00:00 --mem 64000 --wrap "source ~/.bashrc; conda activate $CondaEnv; python calculate_bias.py"; wait;
-    sbatch -W -p ${Partition} -t 2-00:00 --mem 64000 --wrap "source ~/.bashrc; conda activate $CondaEnv; python write_boundary.py"; wait;
-
-    # Replace the days we don't have TROPOMI data with initial GC outputs
-    cp ${workdir}/runGCC1402/OutputDir/GEOSChem.BoundaryConditions.201804{01..29}_0000z.nc4 ${workdir}/smoothed-boundary-conditions
-
+# Remove debug log file if not debug (everything written via  >> debug.log 2>&1)
+if ! ${debug}; then
+    rm "${cwd}/debug.log"
 fi
+
+# Modify and submit the run script
+cp runScriptSamples/operational_examples/harvard_cannon/geoschem.run .
+sed -i -e "s|huce_intel,seas_compute,shared|huce_cascade|g" \
+    -e "s|--mem=15000|--mem=64000|g" \
+    -e "s|-t 0-12:00|-t 07-00:00|g"\
+    -e "s|-c 8|-c 24|g" geoschem.run
+sbatch geoschem.run
