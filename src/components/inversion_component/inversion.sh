41054df17224c478d4f68adb65fb2a0d28a46697
laestrada <laestrada00@gmail.com>
2024-02-07 16:01:54 -0500

    cp cleanup script to run directory

--- src/components/inversion_component/inversion.sh
+++ src/components/inversion_component/inversion.sh
@@ -29,6 +29,7 @@ setup_inversion() {
     cp ${InversionPath}/src/inversion_scripts/utils.py inversion/
     cp ${InversionPath}/src/inversion_scripts/run_inversion.sh inversion/
     cp ${InversionPath}/src/notebooks/visualization_notebook.ipynb inversion/
+    cp ${InversionPath}/src/utilities/cleanup_script.sh .
     sed -i -e "s:{INVERSION_PATH}:${InversionPath}:g" \
            -e "s:{CONFIG_FILE}:${ConfigFile}:g" \
            -e "s:{STATE_VECTOR_ELEMENTS}:${nElements}:g" \
