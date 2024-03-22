2e22ce278349c65ac54f2f28e61c62586aad02e1
James East <47705516+eastjames@users.noreply.github.com>
2024-03-22 07:48:20 -0400

    enable use of CarbonMapper data for state vector aggregation

--- src/inversion_scripts/point_sources.py
+++ src/inversion_scripts/point_sources.py
@@ -8,6 +8,220 @@ from bs4 import BeautifulSoup
 from shapely.geometry import Point
 from dateutil.relativedelta import relativedelta
 
+import numpy as np
+from shapely.geometry import Polygon
+from warnings import warn
+
+
+class PointSources:
+    '''
+    Point source plume and emissions data filtered to ROI.
+
+    Multiple point source datasets can be included and data
+    will be concatenated. Current options include:
+        - SRON plume data
+        - CarbonMapper plume data
+
+    Parameters
+    ----------
+    geofilter: GeoFilter 
+    datasources: list of point source data. Can
+        include CarbonMapper, SRON. Others
+        not yet implemented.
+
+    Use
+    ---
+    ps = PointSources(geofilter, datasources)
+
+    To return a list of [[x,y]] coordinates:
+    ps.get_coords()
+
+    '''
+    def __init__(self, geofilter, datasources):
+
+        self.geofilter = geofilter
+        if (type(datasources) != list):
+            datasources = [datasources]
+        self.datasources = datasources
+        self.points = self._get_points()
+
+    def _get_points(self):
+        # concatenate and return points geometry
+        # within the ROI for all point source datasets 
+        in_points = []
+        for d in self.datasources:
+            # True if point in ROI, else False
+            inout = self.geofilter.filter_points(d.gdf)
+            if inout is not None:
+                in_points.append(d.gdf[inout].geometry)
+        if len(in_points) > 0:
+            gdf_combined = pd.concat(in_points)
+            return gdf_combined.geometry
+        else:
+            return None
+    
+    def get_coords(self):
+        '''
+        Get y,x (lat,lon) coordinates for point sources
+        from all datasets
+         
+        Returns: list of [y,x] coordinates in [lon,lat]
+        '''
+        if self.points is None:
+            return []
+        else:
+            coords = np.column_stack((
+                self.points.y.values,
+                self.points.x.values
+            )).tolist()
+            return coords
+
+
+class GeoFilter:
+    '''
+    Geometry of ROI
+
+    Parameters
+    ----------
+    config: dict of IMI config.yml contents
+
+    Use
+    ---
+    gf = GeoFilter(config)
+
+    To check whether some other point geometry 
+    is in ROI:
+    gf.filter_points()
+
+    '''
+    def __init__(self, config):
+        self.config = config
+        self.geo = self._make_geometry()
+
+    def _make_geometry(self):
+        
+        custom_vectorfile = not self.config["CreateAutomaticRectilinearStateVectorFile"]
+        
+        if custom_vectorfile:
+            shapefile_path = self.config["ShapeFile"]
+            shp_geo = gpd.read_file(shapefile_path).geometry
+            if shp_geo.shape[0] > 1:
+                msg = (
+                    'Shapefile has >1 shape, only using point sources in '
+                    'first shape, please check results'
+                )
+                warnings.warn(msg)
+            geo = shp_geo.iloc[0]
+            
+        else:
+            lon0 = self.config['LonMin']
+            lat0 = self.config['LatMin']
+            lon1 = self.config['LonMax']
+            lat1 = self.config['LatMax']
+            geo = Polygon([
+                [lon0, lat0],
+                [lon1, lat0],
+                [lon1, lat1],
+                [lon0, lat1],
+                [lon0, lat0]
+            ])
+            
+        return geo
+    
+    def filter_points(self, points):
+        '''
+        Return boolean dataframe, True for points
+        in ROI, false for points outside ROI
+         
+        Arguments
+        ---------
+        points: GeoDataFrame
+         
+        Returns: Series with booleans, index
+            matches input GeoDataFrame
+        '''
+        if points.shape[0] == 0:
+            print('No CarbonMapper points found')
+            return None
+            
+        inout = points.within(self.geo)
+        
+        if not inout.any():
+            print('No CarbonMapper points in ROI')
+            return None
+        
+        else:
+            return inout
+
+    
+class SRONPlumes:
+    pass
+
+
+class CarbonMapper:
+    '''
+    CarbonMapper point source data retrieved
+    from the API. Converts data to GeoDataFrame
+    with point geometries.
+
+    Parameters
+    ----------
+    config: dict of IMI config.yml contents
+
+    Use
+    ---
+    cmapper = CarbonMapper(config)
+
+    Access the GeoDataFrame with data:
+    
+    cmapper.gdf
+
+    '''
+    def __init__(self, config):
+        self.config = config
+        self.gdf = self._get_data()
+
+    def _get_data(self):
+        '''
+        api call to carbonmapper
+        '''
+
+        # build query
+        bbox = [
+            self.config['LonMin'],
+            self.config['LatMin'],
+            self.config['LonMax'],
+            self.config['LatMax'],
+        ]
+        q_opts = [
+            'bbox='+'&bbox='.join([str(i) for i in bbox]),
+            'plume_gas=CH4',
+            'status=published',
+            'eps=100', # minimal distance [m], plumes within this distance will be grouped
+            'minpoints=1', # min num plumes in a group
+        ]
+        base_url = 'https://api.carbonmapper.org'
+        endpoint = '/api/v1/catalog/sources.geojson'
+        q_params = '?' + '&'.join(q_opts)
+        url = base_url + endpoint + q_params
+
+        # get data
+        print("Fetching plumes from CarbonMapper...")
+        response = requests.get(url)
+
+        # Raise an exception if the API call returns an HTTP error status
+        response.raise_for_status()  
+        
+        # Process the API response
+        data = response.json()
+
+        # geodataframe from geojson
+        gdf = gpd.GeoDataFrame.from_features(data['features'])
+
+        # is a geodataframe with points as geometry
+        return gdf
+
+
 
 def read_point_source_csv(path):
     """
@@ -60,17 +274,32 @@ def get_point_source_coordinates(config):
 
     # then we read point sources from external datasources
     plumes = []
+    got_plumes = False
     if "PointSourceDatasets" in config.keys():
         if "SRON" in config["PointSourceDatasets"]:
             print("Fetching plumes from SRON database...")
-            plumes = SRON_plumes(config)
-        else:
+            plumes += SRON_plumes(config)
+            got_plumes = True
+        if "CarbonMapper" in config.keys():
+            print("Fetching plumes from CarbonMapper database...")
+            # append CarbonMapper
+            gf = GeoFilter(config)
+            cmapper = CarbonMapper(config)
+            ps = PointSources(gf, cmapper)
+            plumes += ps.get_coords()
+            got_plumes = True
+            inout = gf.filter_points(cmapper.gdf)
+            cmapper.gdf[inout].to_csv('/n/holylfs05/LABS/jacob_lab/Users/jeast/proj/imi/point_source_sv/carbonmapper_sources_testing.csv')
+    
+        if not got_plumes:
             print(
-                'No valid external point source datasets specified. Valid values are: "SRON"'
+                'No valid external point source datasets specified. Valid values are: "SRON", "CarbonMapper"'
             )
 
-    # append SRON
-    return coords + plumes
+    # append point sources
+    coords = coords + plumes
+    
+    return coords
 
 
 def get_plumes(month, year):
@@ -196,5 +425,6 @@ def SRON_plumes(config):
     else:
         plumes = rectangular_filter(plumes, LatMax, LatMin, LonMax, LonMin)
 
+    #plumes.to_csv('plumes_testing.csv')
     plumes_list = plumes[["lat", "lon"]].values.tolist()
     return plumes_list
