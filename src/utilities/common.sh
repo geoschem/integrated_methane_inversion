288d049ca54503d47cff4b1687c872ddeb9e27da
Hannah Nesser <hannah.o.nesser@jpl.nasa.gov>
2024-06-07 17:54:23 -0700

    Continued debugging

--- src/utilities/common.sh
+++ src/utilities/common.sh
@@ -74,17 +74,18 @@ convert_sbatch_to_pbs() {
         echo "    ${f}"
 
         # First, insert needed sites at the top of every file
-        awk -i 'NR==FNR{if (/#SBATCH/) nr=NR; next} {print; if(nr==FNR) print "\nPBS --site-needed=${SitesNeeded}}"}' ${f} ${f}
-        
+        awk -i inplace 'FNR==NR{ if (/^##SBATCH/) p=NR; next} 1; FNR==p{ print "##PBS --site-needed=${SitesNeeded}" }' ${f} ${f}
+        awk -i inplace 'FNR==NR{ if (/^#SBATCH/) p=NR; next} 1; FNR==p{ print "#PBS --site-needed=${SitesNeeded}" }' ${f} ${f}
+
         # Replace SBATCH options
-        sed -i -e "s/PBS -N /PBS -N /g" \
-            -e "s/PBS -l nodes=/PBS -l nodes=/g" \
-            -e "s/PBS -l ncpus=/PBS -l ncpus=/g" \
-            -e "s/PBS -l mem=/PBS -l mem=/g" \
-            -e "s/PBS -l walltime=/PBS -l walltime=/g" \
-            -e "s/PBS -l nodes=1:ppn=/PBS -l nodes=1:ppn=/g" \
-            -e "s/PBS -q /PBS -q /g" \
-            -e "s/PBS -m e/PBS -m e/g" ${f}
+        sed -i -e "s/SBATCH -J /PBS -N /g" \
+            -e "s/SBATCH -N /PBS -l nodes=/g" \
+            -e "s/SBATCH -c /PBS -l ncpus=/g" \
+            -e "s/SBATCH --mem /PBS -l mem=/g" \
+            -e "s/SBATCH -t /PBS -l walltime=/g" \
+            -e "s/SBATCH -n /PBS -l nodes=1:ppn=/g" \
+            -e "s/SBATCH -p /PBS -q /g" \
+            -e "s/SBATCH --mail-type=END/PBS -m e/g" ${f}
     done
 }
 
