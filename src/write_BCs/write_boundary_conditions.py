e8eeeff39afc7bd2e3817571823b11bafe80474f
nicholasbalasus <nicholasbalasus@g.harvard.edu>
2024-06-10 08:54:03 -0400

    update comments

--- src/write_BCs/write_boundary_conditions.py
+++ src/write_BCs/write_boundary_conditions.py
@@ -36,9 +36,8 @@ def get_TROPOMI_times(filename):
 def apply_tropomi_operator_to_one_tropomi_file(filename):
     
     """
-    Run apply_tropomi_operator from src/inversion_scripts/operators/TROPOMI_operator.py for a single TROPOMI file (then saves it to a pkl file)
+    Run apply_tropomi_operator from src/inversion_scripts/operators/TROPOMI_operator.py for a single TROPOMI file
     Example input (str): S5P_RPRO_L2__CH4____20220725T152751_20220725T170921_24775_03_020400_20230201T100624.nc
-    Example output: write the file config["workdir"]/step1/S5P_RPRO_L2__CH4____20220725T152751_20220725T170921_24775_03_020400_20230201T100624_GCtoTROPOMI.pkl
     """
     
     result = apply_tropomi_operator(
@@ -240,8 +239,9 @@ if __name__ == "__main__":
     (2) Make a gridded (2.0 x 2.5 x daily) field of the bias between TROPOMI and GEOS-Chem
         - subtract the TROPOMI and GEOS-Chem grids from part 1 to get a starting point for the bias
         - smooth this field spatially (5 lon grid boxes, 5 lat grid boxes) then temporally (15 days backwards)
+            - use a temporal smoothing of 30 days back in time if there are no data in the past 15 days
         - fill NaN values with the latitudinal average at that time
-            - for a latitudinal average to be defined, there must be >= 30 grid cells at that latitude
+            - for a latitudinal average to be defined, there must be >= 15 grid cells at that latitude
             - when a latitudinal average cannot be found, the closest latitudinal average is used
     (3) Write the boundary conditions
         - using the bias from Part 2, subtract the (GC-TROPOMI) bias from the GC boundary conditions
