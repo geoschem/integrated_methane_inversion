ddca23822641a679905cc8a23430bfb78d9b3716
Lucas A Estrada <63303345+laestrada@users.noreply.github.com>
2024-07-02 13:18:36 -0400

    bugfix for #234 (#235)

--- src/inversion_scripts/invert.py
+++ src/inversion_scripts/invert.py
@@ -150,7 +150,10 @@ def do_inversion(
                 for p in obs_GC[:, 4]
             ]
         )
+        # Define observational errors (diagonal entries of S_o matrix)
+        obs_error = np.power(obs_error, 2)
         gP = s_superO_p**2 / s_superO_1**2
+        # scale error variance by gP
         obs_error = gP * obs_err
 
         # check to make sure obs_err isn't negative, set 1 as default value
@@ -178,9 +181,6 @@ def do_inversion(
             reps = K.shape[0]
             scaling_matrix = np.tile(scale_factors, (reps, 1))
             K *= scaling_matrix
-
-        # Define observational errors (diagonal entries of S_o matrix)
-        obs_error = np.power(obs_error, 2)
 
         # Measurement-model mismatch: TROPOMI columns minus GEOS-Chem virtual TROPOMI columns
         # This is (y - F(xA)), i.e., (y - (K*xA + c)) or (y - K*xA) in shorthand
