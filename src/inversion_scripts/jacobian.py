dd6766cb3ff2f692f12148d3a4ba8d72698b7438
laestrada <laestrada00@gmail.com>
2024-08-08 15:34:54 -0400

    bugfix to argument indexing

--- src/inversion_scripts/jacobian.py
+++ src/inversion_scripts/jacobian.py
@@ -70,20 +70,20 @@ def apply_operator(operator, params, config):
 if __name__ == "__main__":
 
     config = yaml.load(open(sys.argv[1]), Loader=yaml.FullLoader)
-    startday = sys.argv[1]
-    endday = sys.argv[2]
-    lonmin = float(sys.argv[3])
-    lonmax = float(sys.argv[4])
-    latmin = float(sys.argv[5])
-    latmax = float(sys.argv[6])
-    n_elements = int(sys.argv[7])
-    tropomi_cache = sys.argv[8]
-    BlendedTROPOMI = sys.argv[9].lower() == "true"
-    use_water_obs = sys.argv[10].lower() == "true"
-    isPost = sys.argv[11]
-    period_i = int(sys.argv[12])
-    build_jacobian = sys.argv[12]
-    viz_prior = sys.argv[13]
+    startday = sys.argv[2]
+    endday = sys.argv[3]
+    lonmin = float(sys.argv[4])
+    lonmax = float(sys.argv[5])
+    latmin = float(sys.argv[6])
+    latmax = float(sys.argv[7])
+    n_elements = int(sys.argv[8])
+    tropomi_cache = sys.argv[9]
+    BlendedTROPOMI = sys.argv[10].lower() == "true"
+    use_water_obs = sys.argv[11].lower() == "true"
+    isPost = sys.argv[12]
+    period_i = int(sys.argv[13])
+    build_jacobian = sys.argv[14]
+    viz_prior = sys.argv[15]
 
     # Reformat start and end days for datetime in configuration
     start = f"{startday[0:4]}-{startday[4:6]}-{startday[6:8]} 00:00:00"
