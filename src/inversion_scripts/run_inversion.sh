436c9f5bf06b597b29741f5bf41bd3277c00cac4
Melissa Sulprizio <mpayer@seas.harvard.edu>
2025-06-09 12:06:33 -0400

    Fix typo in run_inversion.sh
    Remove extra bracket in file path to config file when calling jacobian.py
    from run_inversion.sh
    
    Signed-off-by: Melissa Sulprizio <mpayer@seas.harvard.edu>

--- src/inversion_scripts/run_inversion.sh
+++ src/inversion_scripts/run_inversion.sh
@@ -153,7 +153,7 @@ else
 
 fi
 
-python jacobian.py ${invPath}}/${configFile} $StartDate $EndDate $LonMinInvDomain $LonMaxInvDomain $LatMinInvDomain $LatMaxInvDomain $nElements $tropomiCache $BlendedTROPOMI $UseWaterObs $isPost $period_i $buildJacobian False; wait
+python jacobian.py ${invPath}/${configFile} $StartDate $EndDate $LonMinInvDomain $LonMaxInvDomain $LatMinInvDomain $LatMaxInvDomain $nElements $tropomiCache $BlendedTROPOMI $UseWaterObs $isPost $period_i $buildJacobian False; wait
 if "$LognormalErrors"; then
     # for lognormal error visualization of the prior we sample the prior run
     # without constructing the jacobian matrix
