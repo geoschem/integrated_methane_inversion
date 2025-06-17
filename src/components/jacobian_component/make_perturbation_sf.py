Fix for Jacobian run setup

src/components/jacobian_component/make_perturbation_sf.py
@@ -211,9 +211,12 @@ def make_perturbation_sf(config, period_number, perturb_value=1e-8):
     )
 
     grid_pert_fpath = os.path.join(archive_dir, f"gridded_pert_scale_{period_number}.nc")
-    make_gridded_perturbation_sf(perturbation_dict["jacobian_pert_sf"], state_vector, grid_pert_fpath)
+    if config['UseGCHP']:
+        make_gridded_perturbation_sf_CSgrid(perturbation_dict["jacobian_pert_sf"], state_vector, grid_pert_fpath)
+    else:
+        make_gridded_perturbation_sf_latlon(perturbation_dict["jacobian_pert_sf"], state_vector, grid_pert_fpath)
 
-def make_gridded_perturbation_sf(pert_vector, statevector, save_pth):
+def make_gridded_perturbation_sf_CSgrid(pert_vector, statevector, save_pth):
     lats = statevector['lats'].values
     lons = statevector['lons'].values
     
@@ -228,7 +231,48 @@ def make_gridded_perturbation_sf(pert_vector, statevector, save_pth):
     
     refyear = 2000
     gridded_pert = xr.DataArray(gridded_pert, dims=['time', 'nf', 'Ydim', 'Xdim'], 
-                                coords=dict(lats=(['nf', 'Ydim', 'Xdim'], lats.values), lons=(['nf', 'Ydim', 'Xdim'], lons.values)),
+                                coords=dict(time=(['time'], [0.]), lats=(['nf', 'Ydim', 'Xdim'], lats.values), lons=(['nf', 'Ydim', 'Xdim'], lons.values)),
+                                attrs=dict(units='1', long_name='scaling factor for state vector elements'))
+    gridded_pert_ds = xr.Dataset({'scale': gridded_pert})
+    # Add attribute metadata
+    gridded_pert_ds.lats.attrs["units"] = "degrees_north"
+    gridded_pert_ds.lats.attrs["long_name"] = "Latitude"
+    gridded_pert_ds.lons.attrs["units"] = "degrees_east"
+    gridded_pert_ds.lons.attrs["long_name"] = "Longitude"
+    gridded_pert_ds['time'].attrs = dict(units='days since {}-01-01 00:00:00'.format(refyear),
+                                        delta_t='0000-01-00 00:00:00', axis='T', standard_name='Time',
+                                        long_name='Time', calendar='standard')
+    gridded_pert_ds['corner_lats'] = statevector['corner_lats']
+    gridded_pert_ds['corner_lons'] = statevector['corner_lons']
+
+    # Save
+    if save_pth is not None:
+        print("Saving file {}".format(save_pth))
+        gridded_pert_ds.to_netcdf(
+            save_pth,
+            encoding={
+                v: {"zlib": True, "complevel": 1} for v in gridded_pert_ds.data_vars
+            },
+        )
+
+    return gridded_pert_ds
+
+def make_gridded_perturbation_sf_latlon(pert_vector, statevector, save_pth):
+    lat = statevector['lat'].values
+    lon = statevector['lon'].values
+    
+    # Map the input vector (e.g., scale factors) to the state vector grid
+    sv_index = np.nan_to_num(statevector.StateVector.values, nan=0).astype(int)
+    gridded_pert = pert_vector[sv_index - 1]
+    gridded_pert = np.where(
+        np.isnan(statevector.StateVector.values),
+        1.0,
+        gridded_pert
+    )
+    
+    refyear = 2000
+    gridded_pert = xr.DataArray(gridded_pert, dims=['time', 'lat', 'lon'], 
+                                coords=dict(time=(['time'], [0.]), lat=(['lat'], lat), lon=(['lon'], lon)),
                                 attrs=dict(units='1', long_name='scaling factor for state vector elements'))
     gridded_pert_ds = xr.Dataset({'scale': gridded_pert})
     # Add attribute metadata