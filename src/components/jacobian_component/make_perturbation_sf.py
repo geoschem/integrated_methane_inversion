Fix for perturbation scaling factor calculation using OptimizeSoil

src/components/jacobian_component/make_perturbation_sf.py
@@ -66,7 +66,7 @@ def update_jacobian_perturbation_files(jacobian_dir, state_vector_labels, flat_s
 
 
 def calculate_perturbation_sfs(
-    state_vector, emis_prior, target_emission=1e-8, prior_sf=None
+    state_vector, emis_prior, target_emission=1e-8, prior_sf=None, OptimizeSoil=False
 ):
     """
     Calculate the perturbation scale factors to perturb each state vector element based on the
@@ -103,7 +103,10 @@ def calculate_perturbation_sfs(
 
     # Calculate perturbation SFs such that applying them to the original
     # emissions will result in a target_emission kg/m2/s2 emission.
-    pert_sf = target_emission / emis_prior["EmisCH4_Total_ExclSoilAbs"]
+    if not OptimizeSoil:
+        pert_sf = target_emission / emis_prior["EmisCH4_Total_ExclSoilAbs"]
+    else:
+        pert_sf = target_emission / emis_prior["EmisCH4_Total"]
 
     # Extract state vector labels
     state_vector_labels = state_vector["StateVector"]
@@ -132,6 +135,8 @@ def calculate_perturbation_sfs(
     # with reaching infinity. Replace any NaN values with 1.0
     max_sf_threshold = 1.5e7
     jacobian_pert_sf[jacobian_pert_sf > max_sf_threshold] = max_sf_threshold
+    if OptimizeSoil:
+        jacobian_pert_sf[jacobian_pert_sf < -max_sf_threshold] = -max_sf_threshold
     jacobian_pert_sf = np.nan_to_num(jacobian_pert_sf, nan=1.0)
 
     # If we are using a kalman filter and have nudged prior emissions,
@@ -167,7 +172,7 @@ def calculate_perturbation_sfs(
     return perturbation_dict
 
 
-def make_perturbation_sf(config, period_number, perturb_value=1e-8):
+def make_perturbation_sf(config, period_number, perturb_value=1e-8, OptimizeSoil=False):
     """
     Calculate the perturbations for each state vector element and update the perturbation files.
     Write out an archive of the flat perturbation scale factors for later use in sensitivity calculations.
@@ -208,7 +213,7 @@ def make_perturbation_sf(config, period_number, perturb_value=1e-8):
 
     # calculate the perturbation scale factors we perturb each state vector element by
     perturbation_dict = calculate_perturbation_sfs(
-        state_vector, hemco_emis, perturb_value, prior_sf
+        state_vector, hemco_emis, perturb_value, prior_sf, OptimizeSoil
     )
 
     # archive npz file of perturbation scale factor dictionary for later calculation of sensitivity
@@ -314,4 +319,4 @@ if __name__ == "__main__":
     perturb_value = perturb_value * 1e-8
 
     config = yaml.load(open(config_path), Loader=yaml.FullLoader)
-    make_perturbation_sf(config, period_number, perturb_value)
+    make_perturbation_sf(config, period_number, perturb_value, config['OptimizeSoil'])