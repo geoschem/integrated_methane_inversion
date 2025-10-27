4a1dea9a072afe049981908779ba2621b0175893
laestrada <laestrada00@gmail.com>
2025-10-27 11:41:32 -0400

    update mapping of default ensemble member based on filtering

--- src/inversion_scripts/invert.py
+++ src/inversion_scripts/invert.py
@@ -441,6 +441,8 @@ def do_inversion_ensemble(
     if filter_ens_members and len(include_ens_members) > 0:
         for k in results_dict.keys():
             results_dict[k] = [results_dict[k][i] for i in include_ens_members]
+        # map idx_default_Ja to new filtered index
+        idx_default_Ja = include_ens_members.index(idx_default_Ja)
     elif len(include_ens_members) == 0:
         print(
             "Warning: No ensemble members with 0.5 <= J_A/n <= 2.0, "
