7715c1c7464bdc92d2fc375af43d8119b2ede745
Ubuntu <ubuntu@ip-10-0-47-150.ec2.internal>
2025-12-19 20:39:48 +0000

    Fix GCHP aws ubuntu script to build spack libraries

--- resources/aws/gchp/ubuntu/install-scripts/install-spack-env.sh
+++ resources/aws/gchp/ubuntu/install-scripts/install-spack-env.sh
@@ -2,49 +2,25 @@
 
 # name: InstallSpackEnvironment
 
-# Name of spack env
+# IMPORTANT: run 'spack load gcc@12.2.0' on the command line prior to running this script.
+
 SpackEnvironmentName="compute_env"
-
-# Spack install spec for desired compiler
 SpackCompiler="gcc@12.2.0"
-
-# Spack environment manifest file
-SpackEnvironmentFile="geoschem_deps-gnu-openmpi-122.yml"
+SpackEnvFile="geoschem_deps-gnu-openmpi-122.yml"
+InstallScriptPath="/home/ubuntu/integrated_methane_inversion/resources/aws/gchp/ubuntu/install-scripts"
 
 # Load spack
 source /home/ubuntu/.bashrc
 
-# Clean up, in case debugging
-spack env rm $SpackEnvironmentName
-
 # Install spack environment
 umask 022
-set -x
-set -e # exit 1 if error
-cat /home/ubuntu/install-scripts/$SpackEnvironmentFile
-spack compiler list
-
-spack env create $SpackEnvironmentName /home/ubuntu/install-scripts/$SpackEnvironmentFile
-spack load $SpackCompiler
-spack env activate $SpackEnvironmentName
-#spack install --fail-fast --show-log-on-error
-spack install -j32 --fail-fast --show-log-on-error target=x86_64 platform=linux os=ubuntu24.04
+spack env create $SpackEnvName $InstallScriptPath/$SpackEnvFile
+spack env activate $SpackEnvName
+spack install -j32 --fail-fast --show-log-on-error
 spack clean --all
-
-##      
-## AutomaticallyLoadEnvironmentOnLogin
-##echo spack load $SpackCompiler >> /etc/bashrc
-##echo spack env activate $SpackEnvironmentName >> /etc/bashrc
-##chmod -R +r /opt/spack
-#
-
-#spack env create $SpackEnvironmentName /home/ubuntu/install-scripts/$SpackEnvironmentFile
-#spack env activate $SpackEnvironmentName
-#spack install --fail-fast --show-log-on-error
-#spack clean --all
 
 # garbage collector to reduce the size of spack install
 spack gc -Ey
-      
+
 # AutomaticallyLoadEnvironmentOnLogin
-echo spack env activate $SpackEnvironmentName >> ~/.bashrc
+echo spack env activate $SpackEnvName >> ~/.bashrc
